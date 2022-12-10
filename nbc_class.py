b9a2024459e6cb3c3fa3ad20f1780c467d665fce
Prajeen Govardhanam <prajeenrg@gmail.com>
2022-12-10 17:29:40 +0530

    refactored code with missing and additional details such as classification report is additional, and pointed the location of dataset into datasets folder

--- nbc_class.py
+++ nbc_class.py
@@ -4,10 +4,10 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import CategoricalNB
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 # load buys_car dataset discussed in class
-df = pd.read_csv('buys_car.csv')
+df = pd.read_csv("datasets/buys_car.csv")
 print("Dataset:")
 print(df.head())
 
@@ -17,6 +17,8 @@ for label in df:
     df[label] = le.fit_transform(df[label])
 print("\nDataset after encoding:")
 print(df.head())
+print("\nData Description:")
+print(df.describe())
 
 # Split dataset into features and target
 print("\nAttributes Split-up:")
@@ -38,3 +40,4 @@ print("Y_predict:", Y_predict)
 # show metrics
 print("\nAccuracy:", accuracy_score(Y, Y_predict))
 print("Confusion Matrix:\n", confusion_matrix(Y, Y_predict))
+print("\nClassification Report:\n", classification_report(Y, Y_predict))
--- nbc_cryo.py
+++ nbc_cryo.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 # load and display dataset
-df = pd.read_excel("Cryotherapy.xlsx")
+df = pd.read_excel("datasets/Cryotherapy.xlsx")
 print("Dataset:")
 print(df.head())
 print("\nData Description:")
@@ -15,7 +15,11 @@ print(df.describe())
 # split train and test
 X = df.iloc[:,:-1]
 Y = df.iloc[:,-1]
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=1, test_size=0.15)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=1, test_size=0.2)
+
+print("\nAttributes Split-up:")
+print("Features:", X.columns.values)
+print("Target:", Y.name)
 
 print("\nNo. of training samples:", len(X_train))
 print("No. of testing samples:", len(X_test))
@@ -34,3 +38,5 @@ print("\nTraining accuracy:", model.score(X_train, Y_train))
 print("Testing accuracy:", accuracy_score(Y_test, Y_predict))
 print("Confusion Matrix:")
 print(confusion_matrix(Y_test, Y_predict))
+print("\nClassification Report:")
+print(classification_report(Y_test, Y_predict))
